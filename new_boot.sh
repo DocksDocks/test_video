5086fc4342c272449f2289e53f4fb8370992d195
DocksDocks <55303379+DocksDocks@users.noreply.github.com>
2022-09-30 16:54:04 -0300

    frajola

---
+++ new_boot.sh
@@ -0,0 +1,25 @@
+sudo apt install vlc
+
+filename=/home/pi/launcher.sh
+if [ ! -f $filename ]
+then
+    touch $filename
+fi
+
+mkdir -p /home/pi/video/
+
+cp eyes.mp4 /home/pi/video/eyes.mp4
+
+printf "#!/bin/bash
+sleep 1
+cvlc -f -L --no-video-title-show /home/pi/video/eyes.mp4" > $filename
+
+
+filenameA=/etc/xdg/lxsession/LXDE-pi/autostart
+if [ ! -f $filenameA ]
+then
+    touch $filenameA
+fi
+
+printf "@bash /home/pi/slide.sh" > $filenameA 
+
